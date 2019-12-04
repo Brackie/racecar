e8ab0ac81b64d6a12a2a6c0738a0bd9f409da419
Brackie <muimikieleko@gmail.com>
2019-12-04 21:02:36 +0300

    Added code to choose random obstacle images

--- obstacle.py
+++ obstacle.py
@@ -2,6 +2,12 @@ import pygame
 from random import randint, choice
 from physics import PAGE_WIDTH, PAGE_HEIGHT, CAR_WIDTH, CAR_HEIGHT, LANE_MARGIN, NO_OF_LANES, RED, GREEN, BLUE, WHITE
 
+car_choices = [
+    "obstacle_1.png",
+    "obstacle_2.png",
+    "obstacle_3.png",
+    "obstacle_4.png"
+]
 
 lane_width = int((PAGE_WIDTH - (LANE_MARGIN * (NO_OF_LANES - 1)))/NO_OF_LANES)
 lane_choices = [
@@ -17,15 +23,17 @@ class Obstacle(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         lane_choice = choice(lane_choices)
-        self.x = randint(lane_choice[0], lane_choice[1])
-        self.y =  randint(0, int(PAGE_HEIGHT * 0.2))
+        image_path = "./resources/{}".format(choice(car_choices))
 
-        self.image = pygame.image.load('./resources/obstacle.png').convert_alpha()
+        self.x = randint(lane_choice[0], lane_choice[1])
+        self.y =  randint(0, int(PAGE_HEIGHT * 0.3))
+
+        self.image = pygame.image.load(image_path).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
 
-        self.yVel = randint(5, 20)
+        self.yVel = randint(5, 10)
 
     def move(self):
         self.rect.move_ip(0, self.yVel)
