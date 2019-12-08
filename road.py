c89d44e99d1b82d8b53f22ad0a213030d58b3b88
Brackie <muimikieleko@gmail.com>
2019-12-08 16:36:05 +0300

    Added accelaration to move function

--- road.py
+++ road.py
@@ -1,5 +1,5 @@
 import pygame
-from physics import PAGE_WIDTH, PAGE_HEIGHT, CAR_WIDTH, CAR_HEIGHT, LANE_MARGIN, COLLISION_ALLOWANCE, NO_OF_LANES, TARMAC, WHITE, BLACK, RED, GREEN, BLUE, WHITE
+from physics import *
 
 
 class Road(pygame.sprite.Sprite):
@@ -10,29 +10,31 @@ class Road(pygame.sprite.Sprite):
         self.gameDisplay = pygame.display.set_mode((PAGE_WIDTH, PAGE_HEIGHT))
         self.road = pygame.Surface((PAGE_WIDTH, PAGE_HEIGHT))
 
+        self.vel = 100
+
         pygame.display.set_caption('A Bit Racey')
 
     def draw(self):
         
         self.gameDisplay.fill(WHITE)
         self.road.fill(WHITE)
+        self.road.fill(BROWN, pygame.Rect(0, 0, LANE_MARGIN, PAGE_HEIGHT))
+        self.road.fill(BROWN, pygame.Rect((PAGE_WIDTH - LANE_MARGIN), 0, LANE_MARGIN, PAGE_HEIGHT))
 
-        lane_width = int((PAGE_WIDTH - (LANE_MARGIN * (NO_OF_LANES - 1)))/NO_OF_LANES)
-        lan2_x_offset = (lane_width + LANE_MARGIN)
-        lan3_x_offset = (lan2_x_offset + lane_width + LANE_MARGIN)
+        lane_width = int((PAGE_WIDTH - (LANE_MARGIN * (NO_OF_LANES + 1)))/NO_OF_LANES)
+        lan2_x_offset = (lane_width + (LANE_MARGIN * 2))
 
-        pygame.draw.rect(self.road, TARMAC, (0, 0, lane_width, PAGE_HEIGHT))
-        pygame.draw.line(self.road, WHITE, (int(lane_width/2), 0), (int(lane_width/2), PAGE_HEIGHT), 2)
+        pygame.draw.rect(self.road, TARMAC, (LANE_MARGIN, 0, lane_width, PAGE_HEIGHT))
+        pygame.draw.line(self.road, WHITE, ((LANE_MARGIN + (int(lane_width/2))), 0), ((LANE_MARGIN + int(lane_width/2)), PAGE_HEIGHT), 2)
 
-        lane2 = pygame.draw.rect(self.road, TARMAC, (lan2_x_offset, 0, lane_width, PAGE_HEIGHT))
+        pygame.draw.rect(self.road, TARMAC, (lan2_x_offset, 0, lane_width, PAGE_HEIGHT))
         pygame.draw.line(self.road, WHITE, ((lan2_x_offset + int(lane_width/2)), 0), ((lan2_x_offset + int(lane_width/2)), PAGE_HEIGHT), 2)
-
-        lane3 = pygame.draw.rect(self.road, TARMAC, (lan3_x_offset, 0, lane_width, PAGE_HEIGHT))
-        pygame.draw.line(self.road, WHITE, ((lan3_x_offset + int(lane_width/2)), 0), ((lan3_x_offset + int(lane_width/2)), PAGE_HEIGHT), 2)
 
         self.gameDisplay.blit(self.road, self.road.get_rect())
         
         pygame.display.update()
 
-    def move(self):
-        self.road.get_rect().move(0, 5)
+    def move(self, a, dt):
+        self.vel = apply_accelaration(self.vel, a, dt)
+        self.dy = int(apply_displacement(self.vel, dt))
+        self.road.scroll(0, self.dy)
